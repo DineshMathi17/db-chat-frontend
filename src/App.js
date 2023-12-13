1c62d3a0b93d27b386d0d94ee5c981af92873fe9
Dinesh <dineshbabum17d@gmail.com>
2023-12-13 13:23:05 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
